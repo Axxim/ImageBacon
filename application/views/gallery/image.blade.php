207b6e1036ff280dfffd3ee885f08bf4fb174248
clone1018 <luke@axxim.net>
2012-12-08 12:26:49 -0600

    Added centering for the image pages

--- application/views/gallery/image.blade.php
+++ application/views/gallery/image.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="well">
+<div class="well center">
 	<a href="https://i.mgba.co/{{$image->file_name}}"><img src="https://i.mgba.co/{{$image->file_name}}" alt="{{ $image->file_name }}"></a>
 </div>
 
